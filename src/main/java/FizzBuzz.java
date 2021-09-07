e8d02eed4e2adc888fdf5e417de641d8e9218e32
Fran Camacho <fcamadi@gmail.com>
2021-09-08 00:34:14 +0200

    refactorization

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,8 +1,6 @@
 public class FizzBuzz {
 
     public String convert(int number) {
-        if (number == 4) return "4";
-        if (number == 2) return "2";
-        return "1";
+        return String.valueOf(number);
     }
 }
--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -1,21 +1,15 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzShould {
 
-    @Test
-    void convert_1_to_1() {
-        assertEquals("1", new FizzBuzz().convert(1));
-    }
-
-    @Test
-    void convert_2_to_2() {
-        assertEquals("2", new FizzBuzz().convert(2));
-    }
-
-    @Test
-    void convert_4_to_4() {
-        assertEquals("4", new FizzBuzz().convert(4));
+    @ParameterizedTest
+    @CsvSource({ "1,1", "2,2", "4,4" })
+    void convert_number_to_FizzBuzz_string(int input, String expectedOutput) {
+        assertEquals(expectedOutput, new FizzBuzz().convert(input));
     }
 }
