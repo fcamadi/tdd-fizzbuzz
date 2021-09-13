398d495bca5481cdf7784d128b11b8122c26d4b7
Fran Camacho <fcamadi@gmail.com>
2021-09-13 22:57:49 +0200

    added convert_from_1_to_15_to_FizzBuzz_string (just for fun)

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,8 +1,7 @@
 public class FizzBuzz {
 
     public String convert(int number) {
-        if (number == 15) return "FizzBuzz";
-        if (number == 30) return "FizzBuzz";
+        if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
         if (number % 5 == 0) return "Buzz";
         if (number % 3 == 0) return "Fizz";
         return String.valueOf(number);
--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -25,13 +25,18 @@ public class FizzBuzzShould {
         assertEquals(expectedOutput, new FizzBuzz().convert(input));
     }
 
-    @Test
-    void convert_15_to_Buzz() {
-        assertEquals("FizzBuzz", new FizzBuzz().convert(15));
+
+    @ParameterizedTest
+    @CsvSource({ "15,FizzBuzz", "30,FizzBuzz", "45,FizzBuzz" })
+    void convert_multiple_of_3_and_5_to_FizzBuzz_string(int input, String expectedOutput) {
+        assertEquals(expectedOutput, new FizzBuzz().convert(input));
     }
 
-    @Test
-    void convert_30_to_Buzz() {
-        assertEquals("FizzBuzz", new FizzBuzz().convert(30));
+
+    @ParameterizedTest
+    @CsvSource({  "1,1", "2,2", "3,Fizz", "4,4", "5,Buzz", "6,Fizz", "7,7", "8,8", "9,Fizz", "10,Buzz",
+                "11,11", "12,Fizz", "13,13", "14,14", "15,FizzBuzz" })
+    void convert_from_1_to_15_to_FizzBuzz_string(int input, String expectedOutput) {
+        assertEquals(expectedOutput, new FizzBuzz().convert(input));
     }
 }
