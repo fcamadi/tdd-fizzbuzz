3a5d34f3faea2acd5e215e52b5076fbf6c6b4523
Fran Camacho <fcamadi@gmail.com>
2021-09-13 22:35:44 +0200

    test convert_10_to_Buzz now ok

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,7 @@ public class FizzBuzz {
 
     public String convert(int number) {
         if (number == 5) return "Buzz";
+        if (number == 10) return "Buzz";
         if (number % 3 == 0) return "Fizz";
         return String.valueOf(number);
     }
